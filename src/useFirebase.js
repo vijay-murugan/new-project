288d735d24483666c57ed71b7a41fcf3edb30c15
vijay-murugan <56683057+vijay-murugan@users.noreply.github.com>
2022-01-16 11:14:21 +0530

    secondcommit

--- src/useFirebase.js
+++ src/useFirebase.js
@@ -1,32 +1,35 @@
 import {app} from "./firebase-config";
 
 import React from 'react'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, TwitterAuthProvider } from "firebase/auth";
 
 function useFirebase() {
     const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+    const provider = new TwitterAuthProvider();
 
     const handleSignIn = () => {
         signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+        .then((result) => {
+          // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+          // You can use these server side with your app's credentials to access the Twitter API.
+          const credential = TwitterAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          const secret = credential.secret;
+      
+          // The signed-in user info.
+          const user = result.user;
+        //   console.log(user);
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.email;
+          // The AuthCredential type that was used.
+          const credential = TwitterAuthProvider.credentialFromError(error);
+          // ...
+        });
     };
     return {
         handleSignIn,
